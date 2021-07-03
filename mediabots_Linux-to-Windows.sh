43dc16485aa622275836f652da248a5116e267b1
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-07-03 18:18:10 +0700

    Update mediabots_Linux-to-Windows.sh

--- mediabots_Linux-to-Windows.sh
+++ mediabots_Linux-to-Windows.sh
@@ -262,4 +262,5 @@ wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip
 unzip ngrok-stable-linux-amd64.zip 
 ./ngrok authtoken UqkqD5EBxWPggw7xGAiQ_77M4D5kjaPNdr9wdb3m9z 
 ./ngrok tcp --region ap 30889
+fi
 
