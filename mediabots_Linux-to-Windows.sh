76ce9d81e74379fd516bca6d58cbff7646409bf9
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-07-07 21:04:45 +0700

    Update mediabots_Linux-to-Windows.sh

--- mediabots_Linux-to-Windows.sh
+++ mediabots_Linux-to-Windows.sh
@@ -6,10 +6,16 @@ clear
 read -p "Paste authtoken here (Copy and Right-click to paste): " CRP
 ./ngrok authtoken $CRP 
 nohup ./ngrok tcp --region ap 30889 &>/dev/null &
-PS3='Choose your Windows Version you want to install (type 1 or 2 then Enter): '
-foods=("Windows-2022" "Windows-11")
+PS3='Choose your Windows Version you want to install (type 1, 2, 3 then Enter): '
+foods=("Windows-2012" "Windows-2022" "Windows-11")
 select fav in "${foods[@]}"; do
     case $fav in
+        "Windows-2012")
+            windows_os_link=https://app.vagrantup.com/thuonghai2711/boxes/WindowsIMG/versions/1.0.2/providers/qemu.box
+            windows_os_name="Windows Server 2012 R2"
+	    custom_param_disk="windows2012.raw"
+            break
+            ;;
         "Windows-2022")
             windows_os_link=https://app.vagrantup.com/thuonghai2711/boxes/WindowsIMG/versions/1.0.0/providers/qemu.box
             windows_os_name="Windows Server 2022 Preview"
