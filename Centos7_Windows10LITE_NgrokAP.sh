d541f2ac3e34ad8831d374bf5539c19ee09ae2c0
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-07-11 18:12:28 +0700

    Update Centos7_Windows10LITE_NgrokAP.sh

--- Centos7_Windows10LITE_NgrokAP.sh
+++ Centos7_Windows10LITE_NgrokAP.sh
@@ -38,7 +38,7 @@ elif [ $dist = "Ubuntu" -o $dist = "Debian" ] ; then
 	# Downloading Portable QEMU-KVM
 	echo "Downloading QEMU"
 	sudo apt-get update
-	yes | sudo apt-get upgrade 
+	DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" upgrade 
 	sudo apt-get install -y qemu-kvm
 	sudo apt-get install -y powershell
 fi
