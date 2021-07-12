a44787f7bc68167fce945093ffec55205dd128a4
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-07-12 17:44:04 +0700

    Update Linux_InstallVM.sh

--- QEMU-KVM/Linux_InstallVM.sh
+++ QEMU-KVM/Linux_InstallVM.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #
 #Vars
-echo Wellcome to VM creation, type CPU,RAM you want:
+echo Wellcome to VM creation, type DISKNAME,CPU,8GBRAM,PORT you want:
 
 read -p "DISKNAME: " DISKNAME
 read -p "CPU: " CPU
-read -p "RAM: " RAM
+##read -p "RAM: " RAM
 read -p "PORT: " PORT
 
 mkdir vm
+cp lite11.qcow2 vm/$DISKNAME
 
 
-
-sudo /usr/bin/qemu-system-x86_64 -nographic -net nic -net user,hostfwd=tcp::30891-:3389 -show-cursor -m 8164M -localtime -enable-kvm -cpu host,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time,+nx -M pc -smp cores=2 -vga std -machine type=pc,accel=kvm -usb -device usb-tablet -k en-us -drive file=/vm/w11.qcow2,index=0,media=disk,format=qcow2 -boot once=d & disown %1
+sudo /usr/bin/qemu-system-x86_64 -nographic -net nic -net user,hostfwd=tcp::$PORT-:3389 -show-cursor -m 8164M -localtime -enable-kvm -cpu host,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time,+nx -M pc -smp cores=$CPU -vga std -machine type=pc,accel=kvm -usb -device usb-tablet -k en-us -drive file=/vm/$DISKNAME.qcow2,index=0,media=disk,format=qcow2 -boot once=d
