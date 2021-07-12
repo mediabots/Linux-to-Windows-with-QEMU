93ce450ed15dbc938fc6f064faedb6e0630164f0
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-07-12 18:31:11 +0700

    Update Linux_InstallVM.sh

--- QEMU-KVM/Linux_InstallVM.sh
+++ QEMU-KVM/Linux_InstallVM.sh
@@ -17,3 +17,4 @@ cp lite11.qcow2 vm/$DISKNAME.qcow2
 sudo /usr/bin/qemu-system-x86_64 -nographic -net nic -net user,hostfwd=tcp::$PORT-:3389 -show-cursor -m 8164M -localtime -enable-kvm -cpu host,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time,+nx -M pc -smp cores=$CPU -vga std -machine type=pc,accel=kvm -usb -device usb-tablet -k en-us -drive file=vm/$DISKNAME.qcow2,index=0,media=disk,format=qcow2 -boot once=d & disown %1
 
 echo Successfully!! Your VM start with RDP Port $PORT
+echo To check Qemu:  ps auxw |grep qemu
