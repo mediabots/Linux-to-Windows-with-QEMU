ed3891fbebc1a7a54d6acad80c4218c58e04c366
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-07-12 17:50:31 +0700

    Update Linux_InstallVM.sh

--- QEMU-KVM/Linux_InstallVM.sh
+++ QEMU-KVM/Linux_InstallVM.sh
@@ -11,7 +11,7 @@ read -p "CPU: " CPU
 read -p "PORT: " PORT
 
 mkdir vm
-cp lite11.qcow2 vm/$DISKNAME
+cp lite11.qcow2 vm/$DISKNAME.qcow2
 
 
 sudo /usr/bin/qemu-system-x86_64 -nographic -net nic -net user,hostfwd=tcp::$PORT-:3389 -show-cursor -m 8164M -localtime -enable-kvm -cpu host,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time,+nx -M pc -smp cores=$CPU -vga std -machine type=pc,accel=kvm -usb -device usb-tablet -k en-us -drive file=/vm/$DISKNAME.qcow2,index=0,media=disk,format=qcow2 -boot once=d
