b7dba06d40b9c14da0d10af6d7963f14d459162a
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-07-13 00:25:00 +0700

    Update Linux_InstallVM.sh

--- QEMU-KVM/Linux_InstallVM.sh
+++ QEMU-KVM/Linux_InstallVM.sh
@@ -16,7 +16,7 @@ cd vm
 qemu-img resize $DISKNAME.qcow2 $custom_disk
 cd ..
 sudo /usr/bin/qemu-system-x86_64 -nographic -net nic -net user,hostfwd=tcp::$PORT-:3389 -show-cursor -m $custom_ram -localtime -enable-kvm -cpu host,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time,+nx -M pc -smp cores=$CPU -vga std -machine type=pc,accel=kvm -usb -device usb-tablet -k en-us -drive file=vm/$DISKNAME.qcow2,index=0,media=disk,format=qcow2 -boot once=d & disown %1
-echo VM Specifications: $CPU CPU , $RAM RAM
+echo VM Specifications: $CPU CPU , $custom_ram RAM
 echo "Successfully!! Your VM start with RDP Port $PORT"
 echo "To check Qemu VM Process:  ps auxw |grep qemu"
 echo "To start NGROK: ./ngrok tcp --region ap $PORT"
