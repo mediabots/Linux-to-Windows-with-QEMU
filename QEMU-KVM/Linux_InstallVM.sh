3a9803c986b8c91c3853d6f0cc3385801f0544d3
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-07-12 23:45:45 +0700

    Update Linux_InstallVM.sh

--- QEMU-KVM/Linux_InstallVM.sh
+++ QEMU-KVM/Linux_InstallVM.sh
@@ -3,15 +3,19 @@
 #Vars
 clear
 echo "Wellcome to VM creation, type DISKNAME,CPU,RAM(MB),PORT(Max 5 number) you want:"
-read -p "DISKNAME: " DISKNAME
+read -p "DISK NAME: " DISKNAME
+read -p "DISK SIZE(Default 10GB): " DISKSIZE
 read -p "CPU(Virtual Processor): " CPU
 read -p "RAM(MB): " RAM
 read -p "PORT(Max 5 number): " PORT
 custom_ram="$RAM""M"
+custom_disk="$DISKSIZE""GB"
 mkdir vm
 cp lite11.qcow2 vm/$DISKNAME.qcow2
+qemu-img resize $DISKNAME.qcow2 $custom_disk
 sudo /usr/bin/qemu-system-x86_64 -nographic -net nic -net user,hostfwd=tcp::$PORT-:3389 -show-cursor -m $custom_ram -localtime -enable-kvm -cpu host,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time,+nx -M pc -smp cores=$CPU -vga std -machine type=pc,accel=kvm -usb -device usb-tablet -k en-us -drive file=vm/$DISKNAME.qcow2,index=0,media=disk,format=qcow2 -boot once=d & disown %1
 echo VM Specifications: $CPU CPU , $RAM RAM
 echo Successfully!! Your VM start with RDP Port $PORT
 echo "To check Qemu VM Process:  ps auxw |grep qemu"
 echo To start NGROK: ./ngrok tcp --region ap $PORT
+echo To start NGROK in background: nohup ./ngrok tcp --region ap $PORT &>/dev/null &
