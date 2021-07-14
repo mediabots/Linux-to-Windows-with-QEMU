dc6d7fc8507e3d741d064cc191a8dc6d33e2d853
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-07-14 20:56:56 +0700

    Update Linux_KillVM.sh

--- QEMU-KVM/Linux_KillVM.sh
+++ QEMU-KVM/Linux_KillVM.sh
@@ -4,6 +4,7 @@
 clear
 echo "Wellcome to VM kill, type DISKNAME of VM you want to kill:"
 cd vm
+ls -l --block-size=GB | cut -f1 -d. | uniq -c
 read -p "DISK NAME: " DISKNAME
 vm=$(echo cat $DISKNAME.txt | bash)
 kill $vm 
