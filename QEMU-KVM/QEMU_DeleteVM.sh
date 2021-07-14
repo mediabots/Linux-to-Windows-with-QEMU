2b6112f9bc379b29ce56b435d45924b940b6ae67
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-07-14 23:12:09 +0700

    Update QEMU_DeleteVM.sh

--- QEMU-KVM/QEMU_DeleteVM.sh
+++ QEMU-KVM/QEMU_DeleteVM.sh
@@ -6,6 +6,8 @@ echo "Wellcome to VM delete, type DISKNAME you want to delete:"
 cd vm 
 ls -l --block-size=GB | cut -f1 -d. | uniq -c
 read -p "DISK NAME: " DISKNAME
+vm=$(echo cat $DISKNAME.txt | bash)
+kill $vm 
 rm  $DISKNAME.qcow2
 rm  $DISKNAME.txt
 cd ..
