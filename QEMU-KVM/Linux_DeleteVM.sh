a70825ffddae7a22b6b0fe6c53dcef49d8ca0823
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-07-14 20:59:55 +0700

    Create Linux_DeleteVM.sh

---
+++ QEMU-KVM/Linux_DeleteVM.sh
@@ -0,0 +1,11 @@
+
+#!/bin/bash
+#
+#Vars
+clear
+echo "Wellcome to VM delete, type DISKNAME you want to delete:"
+cd vm 
+ls -l --block-size=GB | cut -f1 -d. | uniq -c
+read -p "DISK NAME: " DISKNAME
+rm  $DISKNAME.qcow2
+cd ..
