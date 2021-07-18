4b76c2e2506ff796e6161c3a229670ee64f5ef3d
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-07-18 21:59:51 +0700

    Update Katacoda_Windows11SuperLite.sh

--- Katacoda_Windows11SuperLite.sh
+++ Katacoda_Windows11SuperLite.sh
@@ -9,10 +9,8 @@ read -p "Paste authtoken here (Copy and Right-click to paste): " CRP
 ./ngrok authtoken $CRP 
 nohup ./ngrok tcp --region eu 30889 &>/dev/null &
 yum install sudo -y
-sudo yum install wget vim curl genisoimage -y
 echo "Downloading QEMU"
 sudo yum install -y qemu-kvm
-sudo yum install libguestfs-tools -y
 wget -O lite11.qcow2 https://app.vagrantup.com/thuonghai2711/boxes/WindowsQCOW2/versions/1.0.2/providers/qemu.box
 sudo /usr/libexec/qemu-kvm -nographic -net nic -net user,hostfwd=tcp::30889-:3389 -show-cursor -m 6144M -localtime -enable-kvm -cpu host,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time,+nx -M pc -smp cores=4 -vga std -machine type=pc,accel=kvm -usb -device usb-tablet -k en-us -drive file=lite11.qcow2,index=0,media=disk,format=qcow2 -boot once=d & disown %1
 clear
