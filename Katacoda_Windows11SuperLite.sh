de0b864ea7794579fcd8954d45959a65aa954de4
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-07-19 17:59:47 +0700

    Update Katacoda_Windows11SuperLite.sh

--- Katacoda_Windows11SuperLite.sh
+++ Katacoda_Windows11SuperLite.sh
@@ -23,7 +23,7 @@ curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*publ
 echo User: Administrator
 echo Password: Thuonghai001
 echo Script by fb.com/thuong.hai.581
-eho Wait 2-4m VM boot up before connect. 
+echo Wait 2-4m VM boot up before connect. 
 echo Do not close Katacoda tab. VM expired in 1 hour.
 
 
