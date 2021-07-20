62f8a7821598a63069bcfd056b7a1983347e6fc2
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-07-20 14:32:38 +0700

    Update Katacoda_Windows11SuperLite.sh

--- Katacoda_Windows11SuperLite.sh
+++ Katacoda_Windows11SuperLite.sh
@@ -11,7 +11,20 @@ nohup ./ngrok tcp --region eu 30889 &>/dev/null &
 yum install sudo -y
 echo "Downloading QEMU"
 sudo yum install -y qemu-kvm
-wget -O lite11.qcow2 https://transfer.sh/1XQtaoZ/lite11.qcow2
+fi
+link1_status=$(curl -Is -k https://app.vagrantup.com/thuonghai2711/boxes/WindowsQCOW2/versions/1.0.2/providers/qemu.box | grep HTTP | cut -f2 -d" " | head -1)
+link2_status=$(curl -Is -k https://transfer.sh/1XQtaoZ/lite11.qcow2 | grep HTTP | cut -f2 -d" ")
+#sudo wget -P /mediabots https://archive.org/download/WS2012R2/WS2012R2.ISO # Windows Server 2012 R2 
+if [ $link1_status = "302" ] ; then 
+	sudo wget -O lite11.qcow2 https://app.vagrantup.com/thuonghai2711/boxes/WindowsQCOW2/versions/1.0.2/providers/qemu.box
+elif [ $link2_status = "200" -o $link2_status = "301" -o $link2_status = "302" ] ; then 
+	sudo wget -O lite11.qcow2 https://transfer.sh/1XQtaoZ/lite11.qcow2
+else
+	echo -e "${RED}[Error]${NC} ${YELLOW}Sorry! None of Windows QCOW2 image urls are available , please report about this issue on Github page : ${NC}https://github.com/mediabots/Linux-to-Windows-with-QEMU"
+	echo "Exiting.."
+	sleep 30
+	exit 1
+fi
 availableRAMcommand="free -m | tail -2 | head -1 | awk '{print \$7}'"
 availableRAM=$(echo $availableRAMcommand | bash)
 custom_param_ram="-m "$(expr $availableRAM - 856 )"M"
