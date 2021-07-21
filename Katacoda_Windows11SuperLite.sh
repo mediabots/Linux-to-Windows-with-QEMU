1512b3d14e4d5e4bfc4efc6b53af7fe45cd211ea
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-07-22 04:42:11 +0700

    Update Katacoda_Windows11SuperLite.sh

--- Katacoda_Windows11SuperLite.sh
+++ Katacoda_Windows11SuperLite.sh
@@ -21,6 +21,7 @@ custom_param_ram="-m "$(expr $availableRAM - 856 )"M"
 cpus=$(lscpu | grep CPU\(s\) | head -1 | cut -f2 -d":" | awk '{$1=$1;print}')
 nohup sudo /usr/libexec/qemu-kvm -nographic -net nic -net user,hostfwd=tcp::30889-:3389 -show-cursor $custom_param_ram -localtime -enable-kvm -cpu host,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time,+nx -M pc -smp cores=$cpus -vga std -machine type=pc,accel=kvm -usb -device usb-tablet -k en-us -drive file=lite11.qcow2,index=0,media=disk,format=qcow2 -boot once=d &>/dev/null &
 clear
+echo "Katacoda Centos Windows 11 by fb.com/thuong.hai.581"
 echo Your RDP IP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo User: Administrator
