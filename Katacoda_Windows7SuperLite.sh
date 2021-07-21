3b0874cba55f208634116fda0fd896e4b5b88cb9
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-07-22 05:02:27 +0700

    Update Katacoda_Windows7SuperLite.sh

--- Katacoda_Windows7SuperLite.sh
+++ Katacoda_Windows7SuperLite.sh
@@ -14,7 +14,7 @@ sudo yum install -y qemu-kvm
 link1_status=$(curl -Is -k https://app.vagrantup.com/thuonghai2711/boxes/WindowsQCOW2/versions/1.0.2/providers/qemu.box | grep HTTP | cut -f2 -d" " | head -1)
 link2_status=$(curl -Is -k https://transfer.sh/1XQtaoZ/lite11.qcow2 | grep HTTP | cut -f2 -d" ")
 sudo wget -O lite7.qcow2 https://app.vagrantup.com/thuonghai2711/boxes/WindowsQCOW2/versions/1.0.3/providers/qemu.box
-[ -s lite11.qcow2 ] || sudo wget -O lite7.qcow2 https://transfer.sh/1T8pOQ5/win7lite.qcow2
+[ -s lite7.qcow2 ] || sudo wget -O lite7.qcow2 https://transfer.sh/1T8pOQ5/win7lite.qcow2
 availableRAMcommand="free -m | tail -2 | head -1 | awk '{print \$7}'"
 availableRAM=$(echo $availableRAMcommand | bash)
 custom_param_ram="-m "$(expr $availableRAM - 856 )"M"
