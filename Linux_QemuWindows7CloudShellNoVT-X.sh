80c821e9b8e1e827c986da2724d0688203e0fbd1
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-08-09 20:48:16 +0700

    Update Linux_QemuWindows7CloudShellNoVT-X.sh

--- Linux_QemuWindows7CloudShellNoVT-X.sh
+++ Linux_QemuWindows7CloudShellNoVT-X.sh
@@ -1,5 +1,6 @@
 wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+clear
 read -p "Paste authtoken here (Copy and Ctrl+V to paste then press Enter): " CRP
 ./ngrok authtoken $CRP 
 nohup ./ngrok tcp 5900 &>/dev/null &
