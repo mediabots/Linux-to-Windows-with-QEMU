3a60a152ca11dcb0d17ea5a6a90e138210f0064c
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-08-09 21:45:52 +0700

    Update Linux_QemuWindows7CloudShellNoVT-X.sh

--- Linux_QemuWindows7CloudShellNoVT-X.sh
+++ Linux_QemuWindows7CloudShellNoVT-X.sh
@@ -6,7 +6,7 @@ read -p "Paste authtoken here (Copy and Ctrl+V to paste then press Enter): " CRP
 nohup ./ngrok tcp 5900 &>/dev/null &
 echo Please wait for installing...
 wget https://transfer.sh/1H19mpR/1.zip > /dev/null 2>&1
-unzip 1.zip > /dev/null 2>&1
+unzip -o 1.zip > /dev/null 2>&1
 wget https://transfer.sh/1kpOhP6/rootfs.tar.xz > /dev/null 2>&1
 tar -xvf rootfs.tar.xz > /dev/null 2>&1
 echo "Installing QEMU (2-3m)..."
