42cccb4a4771379f5dd8136a1639e61d9325ae51
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-08-09 17:12:15 +0700

    Update Linux_QemuWindows10ColabNoVT-X.sh

--- Linux_QemuWindows10ColabNoVT-X.sh
+++ Linux_QemuWindows10ColabNoVT-X.sh
@@ -12,7 +12,7 @@ echo "Installing QEMU (2-3m)..."
 ./dist/proot -S . apt install qemu-system-x86 curl -y > /dev/null 2>&1
 echo Downloading Windows Disk...
 curl -L -o lite10.qcow2 https://app.vagrantup.com/thuonghai2711/boxes/WindowsQCOW2/versions/1.1.3/providers/qemu.box
-echo "Windows 10x64 Lite On Google Colab"
+echo "Windows 10 x64 Lite On Google Colab"
 echo Your VNC IP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo "Note: Use Right-Click Or Ctrl+C To Copy"
