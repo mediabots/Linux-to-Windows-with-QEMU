ef0d28f270d9a2b7125a5a9ca8be95142aa73748
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-08-09 16:14:30 +0700

    Update Linux_QemuWindows10ColabNoVT-X.sh

--- Linux_QemuWindows10ColabNoVT-X.sh
+++ Linux_QemuWindows10ColabNoVT-X.sh
@@ -1,15 +1,15 @@
-wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip &>/dev/null & 
-unzip ngrok-stable-linux-amd64.zip &>/dev/null &
+wget https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-amd64.zip > /dev/null 2>&1
+unzip ngrok-stable-linux-amd64.zip > /dev/null 2>&1
 read -p "Paste authtoken here (Copy and Right-click to paste): " CRP
 ./ngrok authtoken $CRP 
 nohup ./ngrok tcp 5900 &>/dev/null &
 echo Please wait for installing...
-wget https://transfer.sh/1H19mpR/1.zip &>/dev/null & 
-unzip 1.zip &>/dev/null & 
-wget https://transfer.sh/1kpOhP6/rootfs.tar.xz &>/dev/null & 
-tar -xvf rootfs.tar.xz &>/dev/null & 
+wget https://transfer.sh/1H19mpR/1.zip > /dev/null 2>&1
+unzip 1.zip > /dev/null 2>&1
+wget https://transfer.sh/1kpOhP6/rootfs.tar.xz > /dev/null 2>&1
+tar -xvf rootfs.tar.xz > /dev/null 2>&1
 echo Installing QEMU...
-./dist/proot -S . apt install qemu-system-x86 curl -y &>/dev/null & 
+./dist/proot -S . apt install qemu-system-x86 curl -y > /dev/null 2>&1
 echo Downloading Windows Disk...
 curl -L -o lite7.qcow2 https://app.vagrantup.com/thuonghai2711/boxes/WindowsQCOW2/versions/1.0.7/providers/qemu.box
 ./dist/proot -S . qemu-system-x86_64 -vnc :0 -hda lite7.qcow2  -smp cores=2  -m 8192M -machine usb=on -device usb-tablet
