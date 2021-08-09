fb61d50a5ad9dd4cd1febbfc2e240b62adb5a675
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-08-09 17:15:57 +0700

    Update Linux_QemuWindows10ColabNoVT-X.sh

--- Linux_QemuWindows10ColabNoVT-X.sh
+++ Linux_QemuWindows10ColabNoVT-X.sh
@@ -16,7 +16,7 @@ echo "Windows 10 x64 Lite On Google Colab"
 echo Your VNC IP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo "Note: Use Right-Click Or Ctrl+C To Copy"
-echo "Please Keep Colab Tab Open, Max Time 12h"
+echo "Please Keep Colab Tab Open, Maximum Time 12h"
 echo Script by fb.com/thuong.hai.581
 ./dist/proot -S . qemu-system-x86_64 -vnc :0 -hda lite10.qcow2  -smp cores=2  -m 8192M -machine usb=on -device usb-tablet > /dev/null 2>&1
 
