8b60758692309f53e214f65f8c23708590f41597
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-08-10 17:39:07 +0700

    Update Linux_QemuWindows7LiteDevCloudNoVT-X.sh

--- Linux_QemuWindows7LiteDevCloudNoVT-X.sh
+++ Linux_QemuWindows7LiteDevCloudNoVT-X.sh
@@ -14,11 +14,11 @@ echo "Installing QEMU (2-3m)..."
 ./dist/proot -S . apt install qemu-system-x86 curl -y > /dev/null 2>&1
 echo Downloading Windows Disk...
 curl -L -o lite7.qcow2 https://app.vagrantup.com/thuonghai2711/boxes/WindowsQCOW2/versions/1.0.3/providers/qemu.box
-echo "Windows 7 x86 Lite On Google Cloud Shell"
+echo "Windows 7 x86 Lite On Intel Devcloud"
 echo Your VNC IP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo "Note: Use Right-Click To Copy"
-echo "Please Keep Cloud Shell Tab Open"
+echo "You Can Close Terminal Tab"
 echo Script by fb.com/thuong.hai.581
 cpu=$(echo nproc | bash)
 ./dist/proot -S . qemu-system-x86_64 -vnc :0 -hda lite7.qcow2  -smp cores=2  -m 3072M -machine usb=on -device usb-tablet > /dev/null 2>&1
