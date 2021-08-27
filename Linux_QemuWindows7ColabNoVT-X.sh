16fa827977492d4e1dc8c773e871711acfbd3626
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2021-08-27 17:43:08 +0700

    Update Linux_QemuWindows7ColabNoVT-X.sh

--- Linux_QemuWindows7ColabNoVT-X.sh
+++ Linux_QemuWindows7ColabNoVT-X.sh
@@ -4,12 +4,8 @@ read -p "Paste authtoken here (Copy and Ctrl+V to paste then press Enter): " CRP
 ./ngrok authtoken $CRP 
 nohup ./ngrok tcp 5900 &>/dev/null &
 echo Please wait for installing...
-wget https://transfer.sh/1H19mpR/1.zip > /dev/null 2>&1
-unzip 1.zip > /dev/null 2>&1
-wget https://transfer.sh/1kpOhP6/rootfs.tar.xz > /dev/null 2>&1
-tar -xvf rootfs.tar.xz > /dev/null 2>&1
 echo "Installing QEMU (2-3m)..."
-./dist/proot -S . apt install qemu-system-x86 curl -y > /dev/null 2>&1
+sudo apt install qemu-system-x86 curl -y > /dev/null 2>&1
 echo Downloading Windows Disk...
 curl -L -o lite7.qcow2 https://app.vagrantup.com/thuonghai2711/boxes/WindowsQCOW2/versions/1.0.3/providers/qemu.box
 echo "Windows 7 x86 Lite On Google Colab"
@@ -18,4 +14,4 @@ curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*publ
 echo "Note: Use Right-Click Or Ctrl+C To Copy"
 echo "Please Keep Colab Tab Open, Maximum Time 12h"
 echo Script by fb.com/thuong.hai.581
-./dist/proot -S . qemu-system-x86_64 -vnc :0 -hda lite7.qcow2  -smp cores=2  -m 8192M -machine usb=on -device usb-tablet > /dev/null 2>&1
+sudo qemu-system-x86_64 -vnc :0 -hda lite7.qcow2  -smp cores=2  -m 8192M -machine usb=on -device usb-tablet > /dev/null 2>&1
