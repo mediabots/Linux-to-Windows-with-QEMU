6e44daff553058d74d5d5a00641444a16b30d535
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2022-03-24 12:09:56 +0700

    Update Win10Lite-ngrok.sh

--- Win10Lite-ngrok.sh
+++ Win10Lite-ngrok.sh
@@ -11,7 +11,7 @@ availableRAM=$(echo $availableRAMcommand | bash)
 custom_param_ram="-m "$(expr $availableRAM - 512)"M"
 cpus=$(lscpu | grep CPU\(s\) | head -1 | cut -f2 -d":" | awk '{$1=$1;print}')
 qemu-img resize windows10.raw 80G
-nohup sudo qemu-system-x86_64 -net nic -net user,hostfwd=tcp::3389-:3389 -show-cursor -m 12G -soundhw hda -enable-kvm -cpu host,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time,+nx -M pc -smp cores=$cpus -vga std -machine type=pc,accel=kvm -usb -device usb-tablet -k en-us -drive file=windows10.raw,index=0,media=disk,format=raw,if=virtio -boot once=d -vnc :1 &>/dev/null &
+nohup sudo qemu-system-x86_64 -net nic -net user,hostfwd=tcp::3389-:3389 -show-cursor -m 12G -soundhw hda -enable-kvm -cpu host,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time,+nx -M pc -smp cores=2,threads=2,sockets=1 -vga std -machine type=pc,accel=kvm -usb -device usb-tablet -k en-us -drive file=windows10.raw,index=0,media=disk,format=raw,if=virtio -boot once=d -vnc :1 &>/dev/null &
 clear
 
 echo "======================="
