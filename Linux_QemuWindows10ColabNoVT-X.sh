21addf6a54e4c94cf6028e18aa39d567e2d58c2a
CVaRy <47139574+CVaRy@users.noreply.github.com>
2022-05-02 05:15:34 +0300

    Update Linux_QemuWindows10ColabNoVT-X.sh

--- Linux_QemuWindows10ColabNoVT-X.sh
+++ Linux_QemuWindows10ColabNoVT-X.sh
@@ -15,6 +15,6 @@ curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*publ
 echo "Note: Use Right-Click Or Ctrl+C To Copy"
 echo "Please Keep Colab Tab Open, Maximum Time 12h"
 echo Script by fb.com/thuong.hai.581
-sudo qemu-system-x86_64 -vnc :0 -hda lite10.qcow2  -smp cores=2  -m 8192M -machine usb=on -device usb-tablet > /dev/null 2>&1
+sudo qemu-system-x86_64 -vnc :0 -hda lite10.qcow2  -smp cores=8  -m 10000M -machine usb=on -device usb-tablet > /dev/null 2>&1
 
 
