4034a52a9aba96200d6f76b8b63b73875fe7338c
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2022-08-08 15:18:08 +0700

    Update QEMU_StartVM.sh

--- QEMU-KVM/QEMU_StartVM.sh
+++ QEMU-KVM/QEMU_StartVM.sh
@@ -15,7 +15,7 @@ custom_ram="$RAM""M"
 custom_disk="$DISKSIZE""GB"
 qemu-img resize $DISKNAME.qcow2 $custom_disk
 cd ..
-sudo $qemupath -nographic -net nic -net user,hostfwd=tcp::$PORT-:3389 -show-cursor -m $custom_ram -localtime -enable-kvm -cpu host,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time,+nx -M pc -smp cores=$CPU -vga std -machine type=pc,accel=kvm -usb -device usb-tablet -k en-us -drive file=vm/$DISKNAME.qcow2,index=0,media=disk,format=qcow2 -boot once=d & disown %1
+sudo $qemupath -nographic -net nic -net user,hostfwd=tcp::$PORT-:3389 -show-cursor -m $custom_ram -enable-kvm -cpu host,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time,+nx -M pc -smp cores=$CPU -vga std -machine type=pc,accel=kvm -usb -device usb-tablet -k en-us -drive file=vm/$DISKNAME.qcow2,index=0,media=disk,format=qcow2 -boot once=d & disown %1
 echo $! > $DISKNAME.txt
 cp $DISKNAME.txt vm/$DISKNAME.txt
 rm $DISKNAME.txt
