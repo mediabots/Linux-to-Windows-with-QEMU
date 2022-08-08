5a281eca8bafcf1e8a94049b82801e4755d9ac9f
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2022-08-08 15:13:40 +0700

    Update Linux-W2012R2.sh

--- QEMU-KVM/Linux-W2012R2.sh
+++ QEMU-KVM/Linux-W2012R2.sh
@@ -68,7 +68,7 @@ cp windows2012r2.raw vm/$DISKNAME.raw
 #cd vm
 #qemu-img resize $DISKNAME.raw $custom_disk
 #cd ..
-sudo nohup $qemupath -nographic -net nic -net user,hostfwd=tcp::$PORT-:3389 -show-cursor -m $custom_ram -localtime -enable-kvm -cpu host,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time,+nx -M pc -smp cores=$CPU -vga std -machine type=pc,accel=kvm -usb -device usb-tablet -k en-us -drive file=vm/$DISKNAME.qcow2,index=0,media=disk,format=qcow2 -boot once=d &>/dev/null & disown %1
+sudo nohup $qemupath -nographic -net nic -net user,hostfwd=tcp::$PORT-:3389 -show-cursor -m $custom_ram -localtime -enable-kvm -cpu host,hv_relaxed,hv_spinlocks=0x1fff,hv_vapic,hv_time,+nx -M pc -smp cores=$CPU -vga std -machine type=pc,accel=kvm -usb -device usb-tablet -k en-us -drive file=vm/$DISKNAME.raw,index=0,media=disk,format=raw -boot once=d &>/dev/null & disown %1
 echo $! > $DISKNAME.txt
 cp $DISKNAME.txt vm/$DISKNAME.txt
 rm $DISKNAME.txt
