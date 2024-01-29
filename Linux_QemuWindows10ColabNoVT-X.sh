8097d9d2cb097b355d5121a0fb28e9960d6a02b3
Nguyen Thuong Hai <58414694+kmille36@users.noreply.github.com>
2024-01-30 00:42:31 +0700

    Update Linux_QemuWindows10ColabNoVT-X.sh

--- Linux_QemuWindows10ColabNoVT-X.sh
+++ Linux_QemuWindows10ColabNoVT-X.sh
@@ -8,14 +8,14 @@ sudo apt update -y > /dev/null 2>&1
 echo "Installing QEMU (2-3m)..."
 sudo apt install qemu-system-x86 curl -y > /dev/null 2>&1
 echo Downloading Windows Disk...
-curl -L -o lite10.qcow2 https://app.vagrantup.com/thuonghai2711/boxes/WindowsQCOW2/versions/1.1.3/providers/qemu.box
+curl -L -o w10x64.img https://bit.ly/akuhnetW10x64
 echo "Windows 10 x64 Lite On Google Colab"
 echo Your VNC IP Address:
 curl --silent --show-error http://127.0.0.1:4040/api/tunnels | sed -nE 's/.*public_url":"tcp:..([^"]*).*/\1/p'
 echo "Note: Use Right-Click Or Ctrl+C To Copy"
 echo "Please Keep Colab Tab Open, Maximum Time 12h"
 echo Script by fb.com/thuong.hai.581
-echo Starting Windows x...
-sudo qemu-system-x86_64 -vnc :0 -hda lite10.qcow2  -smp cores=2  -m 8096M
+echo Starting Windows xxx...
+sudo qemu-system-x86_64 -vnc :0 -hda w10x64.img -smp cores=2  -m 8096M
 
 
